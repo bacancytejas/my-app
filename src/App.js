9bab2d4ba3157b6b48d92c1791ab6829a6fc66fe
Binit Ghetiya <binit.ghetiya@bacancytechnology.com>
2019-11-20 18:45:38 +0530

    jest+enzyme test-case

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1 className="App-title">
           Edit
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
